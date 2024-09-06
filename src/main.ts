4f658e5b77e1029ab22455af21da4fef03bedf05
Christopher Roldán Sánchez <rolchrissa@gmail.com>
2024-09-05 23:05:53 -0400

    chore: Rename field in latest document based on user input

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,80 @@
+#!/usr/bin/env node
 import * as dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+import * as readline from "readline";
 dotenv.config();
-(() => {
-  console.log("Hello " + process.env.VAR1);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "Is the document correct? (yes/no): ",
+});
+
+const renameFieldInLatestDocument = async (): Promise<void> => {
+  const uri = process.env.MONGODB_URI || "mongodb://localhost:27017";
+  const dbName = process.env.DB_NAME;
+  const collectionName = process.env.COLLECTION_NAME || "dbName";
+  const dateField = process.env.DATE_FIELD || "cratedAt";
+  const oldField = process.env.OLD_FIELD || "oldField";
+  const newField = process.env.NEW_FIELD || "newField";
+  const validationField = process.env.VALIDATION_FIELD || "_id";
+
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+
+    const database = client.db(dbName);
+    const collection = database.collection(collectionName);
+
+    const recentDocument = await collection
+      .find()
+      .sort({
+        [dateField]: -1,
+      })
+      .limit(1)
+      .toArray();
+
+    if (recentDocument.length > 0) {
+      const document = recentDocument[0];
+      console.log(
+        `Documento encontrado con el ${validationField}: ${
+          document[`${validationField}`]
+        }`
+      );
+
+      let myanswer = false;
+      process.stdout.write("Is the document correct? (yes/no): ");
+      rl.on("line", (answer) => {
+        if (answer.toLowerCase() === "yes" || answer.toLowerCase() === "y") {
+          console.log("The document is correct.");
+          myanswer = true;
+        } else {
+          console.log("The document is not correct.");
+          myanswer = true;
+        }
+        rl.close();
+      });
+      if (myanswer) {
+        await collection.updateOne(
+          { _id: document._id },
+          { $rename: { [oldField]: newField } }
+        );
+
+        console.log("Campo renombrado exitosamente.");
+      }
+    } else {
+      console.log("No se encontró ningún documento.");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  } finally {
+    await client.close();
+  }
+};
+
+(async () => {
+  renameFieldInLatestDocument();
 })();
+
+export { renameFieldInLatestDocument };
+export default renameFieldInLatestDocument;
